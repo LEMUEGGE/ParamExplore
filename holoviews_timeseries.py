f0d6e2677826596f07fc500b97ab925abc392973
LEMUEGGE <jan.mueggenburg@hotmail.com>
2018-05-14 22:27:39 +0200

    create param grid via python interpreter object id to include more d-types

--- holoviews_timeseries.py
+++ holoviews_timeseries.py
@@ -7,33 +7,23 @@ from bokeh.io import curdoc
 import dask as da
 from sklearn.model_selection import ParameterGrid
 from collections import OrderedDict
-"""
-index = np.arange('2005-02', '2006-03', dtype='datetime64[m]')
-df = pd.DataFrame({'data':np.random.rand(index.shape[0]),'data2':np.random.rand(index.shape[0])+5},index=index)
-df['x'] = index
-print(len(df))
+import itertools
+from dask.diagnostics import Profiler, ResourceProfiler, CacheProfiler,visualize
 
-obj = (datashade(hv.Curve(df,vdims=['data2'],kdims=['x'])) * datashade(hv.Curve(df,vdims=['data'],kdims=['x']))).opts(**{'plot' : {'height':400, 'width':1400}})
 
-renderer = hv.renderer('bokeh').instance(mode='server')
-
-document = renderer.get_plot(obj).state
-curdoc().add_root(document)
-
-"""
-
-def param_grid_search(func, dataframe, pos_val_dict):
-    """Given func and a dictionary {1:list, 2:list}, creates a dask function graph for a parallelized function evaluation 
+def param_grid_search(func, param_grid_dict):
+    """Given func and a dictionary with position and iterable parameter states {1:list, 2:list},
+     creates a dask function graph for a parallelized function evaluation 
     within the specified parameter grid"""
-    
-    grid = np.array(np.meshgrid(*[i[1] for i in sorted(pos_val_dict.items())])).reshape(len(pos_val_dict.keys()),-1)
+    param_dict = {id(obj):obj for obj in itertools.chain(*param_grid_dict.values())}
+    param_grid_dict_id = {key : [id(obj) for obj in param_grid_dict[key]] for key in param_grid_dict.keys()} 
+    grid = np.array(np.meshgrid(*[i[1] for i in sorted(param_grid_dict_id.items())])).reshape(len(param_grid_dict_id.keys()),-1)
     
     DAG = OrderedDict()
     for n,state in enumerate(grid.T):
-        task = (func,dataframe)+tuple(state)
-        DAG.update({'grid_n_'+str(state):task})
-        #print(DAG)
-        
+        task = (func,)+tuple([param_dict[key] for key in state])
+        DAG.update({'state_{}'+str(n):task})
+
     return DAG
     
 
@@ -41,12 +31,15 @@ def param_grid_search(func, dataframe, pos_val_dict):
 
 def rolling_mean(df,window_size1,window_size2):
     df_ = df.copy()
-    df_.y = df.y.rolling(window_size1).mean()-df.y.rolling(window_size2).mean()
+    df_.y = df.y.rolling(window_size1).median()-df.y.rolling(window_size2).median()
     
     return df_
     
-DAG = param_grid_search(rolling_mean,pd.DataFrame({'x':np.arange(10000),'y':np.sin(np.arange(10000))+np.random.rand(10000)*0.1}),{2:[10,100,1000],3:[10,100,1000]})
+DAG = param_grid_search(rolling_mean,{1:[pd.DataFrame({'x':np.arange(1000000),'y':np.sin(np.arange(1000000))+np.random.rand(1000000)*0.1}),
+                    pd.DataFrame({'x':np.arange(1000000),'y':np.sin(np.arange(1000000))+30})],2:[10,100,1000,40,20,10,20,4,5],3:[10,100,1000]})
 
+def p(df_list):
+    return
 def plot_df(df_list):
     renderer = hv.renderer('bokeh').instance(mode='server')
     print(df_list)
@@ -54,7 +47,10 @@ def plot_df(df_list):
     linespread  = dynspread(datashade(hv.NdOverlay(lines,     kdims='k')), aggregator=ds.count_cat('k')).opts(**{'plot' : {'height':400, 'width':1400}})
     return renderer.get_plot(linespread).state
 param_states = list(DAG.keys())
-DAG.update({'plot':(plot_df,list(param_states))})
+DAG.update({'plot':(p,list(param_states))})
 
-print(DAG)
-curdoc().add_root(da.get(DAG,'plot'))
+#curdoc().add_root(da.get(DAG,'plot'))
+with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof,CacheProfiler() as cprof:
+    da.multiprocessing.get(DAG,'plot')
+    #da.get(DAG,'plot')
+curdoc().add_root(visualize([prof,rprof,cprof]))
